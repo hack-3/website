761d1f8e5f490f5caac509bca0a744e82fb38b54
Owen Wang <owenwang2007@icloud.com>
2021-01-22 22:31:27 -0500

    Navbar complete (almost)

---
+++ js/nav.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener("click", () => {
+        // Toggle nav (for mobile view)
+        nav.classList.toggle("nav-active");
+
+        // Animate text in
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+
+        // Burger animation
+        burger.classList.toggle("toggle");
+    });
+}
+
+navSlide();
