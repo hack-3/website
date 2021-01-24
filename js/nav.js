881fc4e19c3bf387253ab79edc0fe1b684c29019
Owen Wang <owenwang2007@icloud.com>
2021-01-24 14:03:17 -0500

    Add meta information

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-const navSlide = () => {
+function navSlide() {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
