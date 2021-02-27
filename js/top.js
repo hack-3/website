427f2491ec0606c93a6e89e337e7c9685ea16039
Owen Wang <owenwang2007@icloud.com>
2021-02-27 18:44:33 -0500

    Improve up button

--- js/top.js
+++ js/top.js
@@ -2,19 +2,10 @@
 topButton = document.getElementById("top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
+window.onscroll = () => {
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        topButton.style.display = "block";
-        topButton.style.animation = "fadeIn 0.2s ease forwards";
+        topButton.style.opacity = "1";
     } else {
-        topButton.style.animation = "fadeOut 0.2s ease forwards";
+        topButton.style.opacity = "0";
     }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+};
