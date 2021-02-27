80ad410db7156aed40d43177b72c38ba758ae006
Owen Wang <owenwang2007@icloud.com>
2021-02-26 20:52:41 -0500

    Add to top button

--- js/navSlide.js
+++ js/navSlide.js
@@ -13,7 +13,7 @@ function navSlide() {
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
 
