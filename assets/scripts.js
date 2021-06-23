7fd642f02ae6c6336db1ad0eb5fcd470c12fecad
AbhisarAnand <abhisar.muz@gmail.com>
2021-06-22 20:28:19 -0400

    add the incrementing numbers on the statistics page

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,3 +9,34 @@
 window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('fade-out');
 });
+
+// Incrementing Statistics Script
+var a = 0;
+$(window).scroll(function () {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function () {
+            var $this = $(this), countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+                {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        this.countNum = this.countNum + "+"
+                        $this.text(this.countNum);
+                        // alert('finished');
+                    }
+                });
+        });
+        a = 1;
+    }
+
+});
